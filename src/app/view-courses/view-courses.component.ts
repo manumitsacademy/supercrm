e0f456b28addfb1dea5eb0aa4f4b3c57676eeded
nishantrane <ranenishant31@gmail.com>
2018-03-06 14:42:05 +0530

    Editcourse

--- src/app/view-courses/view-courses.component.ts
+++ src/app/view-courses/view-courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-view-courses',
@@ -10,13 +11,18 @@ export class ViewCoursesComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) { }
   courses;
+  Apikey= "?apiKey=ZShQtoghVlx_ZFHYiZa6I0dRCgdoGr--"
   ngOnInit() {
     
-    this.httpClient.get("https://api.mlab.com/api/1/databases/demo/collections/test?apiKey=-Ky8Wk0gH3HSexYngXpRI6Wg2itAnavf")
+    this.httpClient.get("https://api.mlab.com/api/1/databases/academy_crm/collections/Courses/"+this.Apikey)
                     .subscribe((d)=>{
                         this.courses=d;
                         console.log(d);
                     });
+  }
+  editcourse(name){
+    console.log("Reached here");
+    console.log(name);
   }
 
 }
